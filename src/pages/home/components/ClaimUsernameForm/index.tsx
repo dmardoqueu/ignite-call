919d26b96be37e656b022b3ab4ba9993195e24fb
dmardoqueu <diogomardoqueu@gmail.com>
2025-06-30 20:36:39 -0300

    Dando início a autenticação oAuth com o Google Calendar

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,23 +1,30 @@
 import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
 import { useRouter } from 'next/router'
 
-const claimUsernameFormSchema = z.object({
-    username: z.string()
-        .min(3, { message: 'O username precisa ter ao menos 3 letras' })
-        .regex(/([a-z\\-]+)$/i, { message: 'O username pode conter apenas letras e hifens' })
-        .transform((username) => username.toLowerCase())
+const ClaimUsernameFormSchema = z.object({
+    username: z
+        .string()
+        .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+        .regex(/^([a-z\\-]+)$/i, {
+            message: 'O usuário pode ter apenas letras e hifens.',
+        })
+        .transform((username) => username.toLowerCase()),
 })
 
-type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<ClaimUsernameFormData>({
-        resolver: zodResolver(claimUsernameFormSchema)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<ClaimUsernameFormData>({
+        resolver: zodResolver(ClaimUsernameFormSchema),
     })
 
     const router = useRouter()
@@ -25,7 +32,7 @@ export function ClaimUsernameForm() {
     async function handleClaimUsername(data: ClaimUsernameFormData) {
         const { username } = data
 
-        await router.push(`/register?username=${username}`)
+        router.push(`/register?username=${username}`)
     }
 
     return (
@@ -35,15 +42,19 @@ export function ClaimUsernameForm() {
                     size="sm"
                     prefix="ignite.com/"
                     placeholder="seu-usuário"
-                    {...register('username')} />
+                    {...register('username')}
+                />
                 <Button size="sm" type="submit" disabled={isSubmitting}>
                     Reservar
                     <ArrowRight />
                 </Button>
             </Form>
+
             <FormAnnotation>
                 <Text size="sm">
-                    {errors.username ? errors.username.message : 'Digite o nome do usuário desejado'}
+                    {errors.username
+                        ? errors.username.message
+                        : 'Digite o nome do usuário desejado'}
                 </Text>
             </FormAnnotation>
         </>
